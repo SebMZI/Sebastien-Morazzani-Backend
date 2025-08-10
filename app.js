347e21239c33e42078e7fbfd126bdf86d738167c
SebMZI <mrgy.sebastien@gmail.com>
2025-08-10 12:29:00 +0200

    feat: initialize server

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require("express")
-const app = express()
+require("./config/env.config");
+const express = require("express");
+const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("Hello, World!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
