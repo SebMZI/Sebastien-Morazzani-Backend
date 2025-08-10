d5321ee2d47ab40615617e7e7afc902749c2e53f
SebMZI <mrgy.sebastien@gmail.com>
2025-08-10 13:20:59 +0200

    feat: implemente connectToDatabase function at the server start

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("./config/env.config");
 const express = require("express");
+const { connectToDatabase } = require("./config/mongodb.config");
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  await connectToDatabase();
 });
