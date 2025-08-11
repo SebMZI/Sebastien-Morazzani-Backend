9603abf42e3946be89c3b3fce6b464760e10fa41
SebMZI <mrgy.sebastien@gmail.com>
2025-08-11 12:23:47 +0200

    fix: plugin js missing in eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,15 @@
 import js from "@eslint/js";
-import globals, { jest } from "globals";
+import globals from "globals";
 import { defineConfig } from "eslint/config";
+
+const { jest } = globals;
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js, jest },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: { globals: { ...globals.node, ...globals.jest } },
   },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
 ]);
