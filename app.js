82c6777b265cc14c072266f9bee2974ba88c983d
SebMZI <mrgy.sebastien@gmail.com>
2025-08-24 11:10:13 +0200

    feat: add express.json() middleware

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const { connectToDatabase } = require("./config/mongodb.config");
 const projectRouter = require("./routes/project.routes");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// Middlewares
+app.use(express.json());
 
+// Routes
 app.use("/api/v1/projects", projectRouter);
 
+// Server
 app.listen(process.env.PORT, async () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
   await connectToDatabase();
